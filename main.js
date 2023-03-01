88e1178544e982aa5abe76de795d1171db31434e
Alvin <alvinroeiv@gmail.com>
2023-02-28 20:13:00 -0500

    Completed functionality of Etch-a-Sketch and clamped values of grid size

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const title = document.querySelector('h1');
-const container = document.querySelector('.container');
+const container = document.querySelector('.container'); //The parent of the container of the grid. It is a column of rows, which are filled with the divs that form the grid.
 
-let size = prompt('Enter a number up to 100');
+let size = Math.max(Math.min(prompt('Enter a number between 10 and 100'), 100), 10);
 let last = container; 
-//Create a flex div row for each row
+//Creating a Grid from Divs. Each individual square is called a "cell". They fill rows. The rows are all contained in one container which is a flex container set to column.
 for(let i = 0; i < size; i++)
 {
     const row = document.createElement('div');
@@ -13,9 +13,15 @@ for(let i = 0; i < size; i++)
     {
         const cell = document.createElement('div');
         cell.classList += 'cell';
+        cell.addEventListener('mouseover', e => onMouseOver(e));
         row.appendChild(cell);
     }
 
     container.appendChild(row);
+}
+
+function onMouseOver(mouseOverEvent) {
+    console.log(mouseOverEvent.srcElement);
+    mouseOverEvent.srcElement.classList.add('drawn');
 }
 
